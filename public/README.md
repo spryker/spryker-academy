349265a7719e8a473d5b1c94e5b9a41740bf97d1
ilmente <alessandro.bellini@spryker.com>
2017-06-21 17:46:48 +0200

    update site structure

--- public/README.md
+++
@@ -1,2 +0,0 @@
-# spryker-academy
-The central hub for Spryker documentation, knowledge and information.
--- public/resources/Scripts/require.config.js
+++ public/resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=636336527976555075'
+    urlArgs: 't=636336602358033756'
 });
