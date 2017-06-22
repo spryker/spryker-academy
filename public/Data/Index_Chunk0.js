4aea1a9ee7d4ef3b39390d0f1c7cc7643a087014
Alessandro Bellini <alessandro.bellini@spryker.com>
2017-06-22 11:50:54 +0200

    staging version 3

--- public/Data/Index_Chunk0.js
+++ public/Data/Index_Chunk0.js
@@ -1,1 +1,1 @@
-define({'Tables':{l:[{u:'/print_only_topics/appendix.html#kanchor1',t:'Appendix'}]}});
+define({});
--- public/resources/Scripts/require.config.js
+++ public/resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=636336889453359852'
+    urlArgs: 't=636337289635358511'
 });
