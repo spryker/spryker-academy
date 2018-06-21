96907f9b3824ae03932cd7b29c3477e7b0308554
oksana <gibmirmilch@gmail.com>
2018-06-21 16:54:08 +0300

    Merge branch 'master' of https://github.com/spryker/spryker-academy

--- public/resources/Scripts/require.config.js
+++ public/resources/Scripts/require.config.js
@@ -1,7 +1,3 @@
 require.config({
-<<<<<<< HEAD
-    urlArgs: 't=636651950151759799'
-=======
-    urlArgs: 't=636651317160322255'
->>>>>>> 3cfe53fe6fc6d7a77a71e9196d3b962f38a55af4
+    urlArgs: 't=636651962095164631'
 });
